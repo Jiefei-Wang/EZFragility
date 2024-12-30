19dd34feac985f57421a9cc2307634b53bdbfbba
Ioannis Malagaris <ymalag@gmail.com>
2024-12-30 14:01:39 -0600

    Test Coverage > 90%
    Create objects in Mac server |>
    Copied to project (see "data-raw/IoannisPt1.R") |>
    use them in tests to achieve high coverage

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -14,10 +14,10 @@ test_that("calc_adj_frag works", {
     expect_no_error()
 })
 
-# test_that("frag_quantile works", {
-#   frag_quantile(repository, f, t_window, t_step, soz, sozc) |> expect_no_error()
-#   mean_f_calc(repository, f, soz, sozc) |> expect_no_error()
-#   threshold_buckets(f, c(0.1, 0.5, 1)) |> expect_no_error()
-#   threshold_fragility(repository, adj_frag_info, t_step, 0.1, 1) |>
-#     expect_no_error()
-# })
+test_that("frag_quantile works", {
+  frag_quantile(repository, f, t_window, t_step, soz, sozc) |> expect_no_error()
+  mean_f_calc(repository, f, soz, sozc) |> expect_no_error()
+  threshold_buckets(f, c(0.1, 0.5, 1)) |> expect_no_error()
+  threshold_fragility(repository, adj_frag_info, t_step, 0.1, 1) |>
+    expect_no_error()
+})
